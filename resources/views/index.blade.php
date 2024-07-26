c8936d790cc5a0c081e0e468259b6a0b0f939061
sadia <sadia.ria4@gmail.com>
2024-07-26 11:46:21 +0600

    add new route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,15 @@
-<div>
-Hello I am blade<br/>
-</div>
+<h1>
+List of Task<br/>
+</h1>
 
-@isset($name)
-This is : {{$name}}
-@endisset
+<div>
+    <!-- @if(count($tasks)) -->
+    @forelse ($tasks as $task )
+    <a href="{{route('tasks.show', ['id' => $task->id])}}">{{$task->title}}</a><br/>
+    @empty
+      <div>no task</div>
+    @endforelse
+    <!-- @else
+    <div>no task</div> -->
+    <!-- @endif  -->
+</div>
